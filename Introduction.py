849401c90d3128d1c1754e2d10c880c5d99f0c42
Sabine Wren <pandyaved1999@gmail.com>
2020-09-24 18:00:44 +0530

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,2 @@
+print("Hello! Welcome to the world of Pythonh Programming. This is Vedant Pandya here.")
+print("I am practicing and refreshing my Python knowledge and skills again from scratch but this time with documentation.")
